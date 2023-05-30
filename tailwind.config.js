18b1d9bed186ffc6579f3d83129d671416938f6a
jeonbyeongmin <qudals7613@gmail.com>
2023-05-31 01:40:56 +0900

    feat: update tailwind grayscale

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          50: '#FAFAFA',
+          100: '#F4F4F5',
+          200: '#E4E4E7',
+          300: '#D4D4D8',
+          400: '#A1A1AA',
+          500: '#71717A',
+          600: '#52525B',
+          700: '#3F3F46',
+          800: '#27272A',
+          900: '#18181B',
+          950: '#0F0F12',
+        },
+      },
       boxShadow: {
         dialog: '0 16px 70px rgba(0, 0, 0, 0.2);',
       },

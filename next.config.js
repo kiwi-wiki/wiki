b07620927a557ce7a1e0a82dbf31ccd20f6e7337
jeonbyeongmin <qudals7613@gmail.com>
2023-06-01 22:56:24 +0900

    build: prepare deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: 'wiki',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
